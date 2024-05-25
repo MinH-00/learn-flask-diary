749fce58c0196652621d174a1bb8fb92e3345bad
ymh0977 <144010663+ymh0977@users.noreply.github.com>
2024-05-25 14:44:49 +0900

    08 DB연결 준비, SqlAlchemy

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from diary import create_app
 app = create_app()
 
 # run
-if __name__ == '__main__':
+if __name__ == '__main__':#파일의 시작점일때
     app.run(debug=True)
--- diary/__init__.py
+++ diary/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create DB connection
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 # create App
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'semicircle_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     #import Blueprint
     from .views import views
