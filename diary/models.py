d9ed4736922ab4bbdbaf77cb881efa0e9ac7280a
ymh0977 <144010663+ymh0977@naver.com>
2024-05-25 15:08:15 +0900

    10 외래키(참조키, ForeignKey)

--- diary/models.py
+++ diary/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     nickname = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200))
+    notes = db.relationship('Note')
 
 #define Note Model
 class Note(db.Model):
@@ -15,3 +16,4 @@ class Note(db.Model):
     title = db.Column(db.String(50))
     content = db.Column(db.String(2000))
     datetime = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
